73e06a3d77034b8744dc6199c2021bb7d5a20a87
lovekeshpal <lovekeshfgr@gmail.com>
2023-09-22 22:40:22 +0530

    Started with Strings

---
+++ src/strings/CharAtIndex.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class CharAtIndex {
+    public static void main(String[] args)
+    {
+        String str = "Learn Java";
+        System.out.println("The original string = " + str);
+        // To get the character at positions 0 and 9.
+        int index1 = str.charAt(0);
+        int index2 = str.charAt(9);
+
+        // Printing the results.
+        System.out.println("The character at position 0 is " +
+                (char)index1);
+        System.out.println("The character at position 10 is " +
+                (char)index2);
+    }
+}
